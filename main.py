0de7b280fec28a986546962d886bb62f022ed9d6
asuresh99 <76408267+asuresh99@users.noreply.github.com>
2022-12-06 21:38:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,57 @@ def index():
     return render_template('index.html', sql_table = log, table_title=col_names)
 
 
+
+@app.route('/api/update_basket_a')
+# this is how you define a function in Python
+def updateBasket():
+    # this is your index page
+    # connect to DB
+    cursor, connection = util.connect_to_db(username,password,host,port,database)
+    # execute SQL commands
+    record = util.run_and_commit_sql(cursor, connection, "INSERT INTO basket_a VALUES (5, 'Cherry')")
+    if record == -1:
+        # you can replace this part with a 404 page
+        print('Something is wrong with the SQL command')
+    else:
+        # this will return all column names of the select result table
+        # ['customer_id','store_id','first_name','last_name','email','address_id','activebool','create_date','last_update','active']
+        # col_names = [desc[0] for desc in cursor.description]
+        # only use the first five rows
+        # log = record[:5]
+        log = 'Success!'
+        connection.commit()
+    util.disconnect_from_db(connection,cursor)
+    # using render_template function, Flask will search
+    # the file named index.html under templates folder
+    return render_template('api/update_basket_a.html', log_html = log)
+
+@app.route('/api/unique')
+# this is how you define a function in Pyth#on
+def unique():
+    # this is your index page
+    # connect to DB
+    cursor, connection = util.connect_to_db(username,password,host,port,database)
+    # execute SQL commands
+    record = util.run_and_fetch_sql(cursor, "(select distinct fruit_a, fruit_b from basket_a full outer join basket_b on basket_a.fruit_a = basket_b.fruit_b where basket_a.a is NULL or basket_b.b is NULL)")
+    if record == -1:
+        # you can replace this part with a 404 page
+        print('Something is wrong with the SQL command')
+    else:
+        # this will return all column names of the select result table
+        # ['customer_id','store_id','first_name','last_name','email','address_id','activebool','create_date','last_update','active']
+        col_names = [desc[0] for desc in cursor.description]
+        # only use the first five rows
+        log = record[:5]
+        # log=[[1,2],[3,4]]
+    # disconnect from database
+    util.disconnect_from_db(connection,cursor)
+    # using render_template function, Flask will search
+    # the file named index.html under templates folder
+    return render_template('api/unique.html', sql_table = log, table_title=col_names)
+
+
+
 if __name__ == '__main__':
 	# set debug mode
     app.debug = True
